68bcb316769314d5cbab681fac7cb89798a7144d
Tiago101010 <tiagovsantos36@gmail.com>
2025-10-06 16:42:08 -0300

    Minha primeira Api

---
+++ Api Rick and morty/main.py
@@ -0,0 +1,14 @@
+import requests
+
+def fetch_data(endpoint, filters = {}):
+    url = f"https://rickandmortyapi.com/api/{endpoint}"
+    response = requests.get(url, params = filters)
+    
+    return response.json() if response.status_code == 200 else None
+
+character = fetch_data("character", {"name":"Rick"})
+    
+if character:
+    print(character)
+else:
+    print("failed to fetch data")
