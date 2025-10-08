737ba0473cb714338a00944ed0a7aca7f9f7e499
Tiago101010 <tiagovsantos36@gmail.com>
2025-10-08 01:54:22 -0300

    up1

--- Api Rick and morty/main.py
+++ Api Rick and morty/main.py
@@ -6,7 +6,7 @@ def fetch_data(endpoint, filters = {}):
     
     return response.json() if response.status_code == 200 else None
 
-character = fetch_data("character", {"name":"Rick"})
+character = fetch_data("character", {"name":"Morty"})
     
 if character:
     print(character)
